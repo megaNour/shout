3bc122e6080edbdada79ed78931ecc51db942684
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-05 19:21:15 +0100

    fix the bench using outdated API

--- bench.sh
+++ bench.sh
@@ -2,10 +2,9 @@
 
 eval "$(shoutctl source 2>/dev/null)"
 
-ITERATIONS=100000
+ITERATIONS=100000 #100k
 
 _shout_bench_loop() {
-  echo "$@"
   _shout_bench_i=0
   while [ "$_shout_bench_i" -lt "$ITERATIONS" ]; do
     _shout_bench_i=$((_shout_bench_i + 1))
@@ -14,15 +13,15 @@ _shout_bench_loop() {
 }
 
 _shout_bench_shout() {
-  shout "$1" "This is a grey log. Log arguments inline in grey. Notice the empty OPT_STRING."
+  shout "$1" "This is a grey log. Log arguments inline in grey."
 }
 
 _shout_bench_shoutf() {
-  shoutf "This is a grey log. Log arguments inline in grey. Notice the empty OPT_STRING."
+  shoutf "This is a grey log. Log arguments inline in grey."
 }
 
 _shout_bench_printf() {
-  printf "%s\n" "${_gry}This is a grey log. Log arguments inline in grey. Notice the empty OPT_STRING.$_res" 1>&2
+  printf "%s\n" "${_gry}This is a grey log. Log arguments inline in grey." 1>&2
 }
 
 _shout_bench_test="_shout_bench_$1"
@@ -31,4 +30,4 @@ shift
 _shout_bench_loop "$_shout_bench_test" "$@"
 
 printf "\n"
-shout "" "${_grn}benching '${_shout_bench_test#_shout_bench_}'"
+shoutf "${_grn}benched '${_shout_bench_test#_shout_bench_}'"
