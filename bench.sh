8d3024d18fc8614725b4481464ede26de57c15d1
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-05 09:55:42 +0100

    improve the bench...
    - now takes a bench method
    - has a base loop method
    - methods have been normalized to one method repeated per bench
    - runner is normalized to dash

---
+++ bench.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env dash
+
+eval "$(shoutctl source 2>/dev/null)"
+
+ITERATIONS=10000
+
+_shout_bench_loop() {
+  _shout_bench_i=0
+  while [ "$_shout_bench_i" -lt "$ITERATIONS" ]; do
+    _shout_bench_i=$((_shout_bench_i + 1))
+    $1
+  done
+}
+
+_shout_bench_shout() {
+  shout "" "This is a grey log. Log arguments inline in grey. Notice the empty OPT_STRING."
+}
+
+_shout_bench_printf() {
+  printf "%s\n" "${_gry}This is a grey log. Log arguments inline in grey. Notice the empty OPT_STRING.$_res" 1>&2
+}
+
+while [ "$#" -gt 0 ]; do
+  _shout_bench_test="_shout_bench_$1"
+  shift
+  _shout_bench_loop "$_shout_bench_test"
+  printf "\n"
+  shout "" "${_grn}benching '${_shout_bench_test#_shout_bench_}'"
+done
