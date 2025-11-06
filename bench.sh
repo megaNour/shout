7b2567197d0435d6008012ff48e5e3ccfcf40c5f
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-06 13:25:35 +0100

    improve bench with a guarded printf method

--- bench.sh
+++ bench.sh
@@ -20,8 +20,15 @@ _shout_bench_shoutf() {
   shoutf "This is a grey log. Log arguments inline in grey."
 }
 
+_shout_bench_printfg() {
+  _shout_level=${1:?shout requires a log level. Use shoutf to force.}
+  [ -n "$SHOUT_DISABLED" ] && return 0
+  [ "$((SHOUT_LEVEL - _shout_level))" -ge 0 ] && shift || return 0
+  printf "%s%s%s\n" "$SHOUT_COLOR" "This is a grey log. Log arguments inline in grey." "$_res" >&2
+}
+
 _shout_bench_printf() {
-  printf "%s\n" "${_gry}This is a grey log. Log arguments inline in grey." 1>&2
+  printf "%s%s%s\n" "$SHOUT_COLOR" "This is a grey log. Log arguments inline in grey." "$_res" >&2
 }
 
 _shout_bench_test="_shout_bench_$1"
